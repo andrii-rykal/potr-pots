47f728492f22ed280afa74e6531d605ed4bd3eb9
Andrii Rykal <andreuka79@gmail.com>
2024-04-17 12:40:13 +0300

    make build

---
+++ index.0ff8af88.js
@@ -0,0 +1,2 @@
+window.addEventListener("hashchange",function(){"#menu"===window.location.hash?document.body.classList.add("page__body--with-menu"):document.body.classList.remove("page__body--with-menu")});var e=document.querySelector(".questions__form");e.addEventListener("submit",function(n){n.preventDefault(),e.reset()});
+//# sourceMappingURL=index.0ff8af88.js.map
